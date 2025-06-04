84454fbab16846bae60429d251bf5139c89923aa
xot1990 <47980618+xot1990@users.noreply.github.com>
2025-06-04 17:10:32 +0300

    1

---
+++ Assets/Scripts/CoffeMachine.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeMachine : MonoBehaviour
+{
+		public bool open;
+		public Transform Player;
+		public ParticleSystem Psys1;
+		public ParticleSystem Psys2;
+		
+		void Start()
+		{
+			open = false;
+		}
+
+		void OnMouseOver()
+		{
+			{
+				if (Player)
+				{
+					float dist = Vector3.Distance(Player.position, transform.position);
+					if (dist < 15)
+					{
+						if (open == false)
+						{
+							if (Input.GetMouseButtonDown(0))
+							{
+								open = true;
+								Psys1.Play();
+								Psys2.Play();
+							}
+						}
+						else
+						{
+							if (Input.GetMouseButtonDown(0))
+							{
+								open = false;
+								Psys1.Play();
+								Psys2.Play();
+							}
+
+						}
+
+					}
+				}
+
+			}
+
+		}
+
+}
